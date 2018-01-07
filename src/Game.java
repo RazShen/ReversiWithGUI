85971e7ef0960b1b9f0d241309e3a31f9652022b
tomergrady <tomergrady@gmail.com>
2018-01-07 19:42:29 +0200

    game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,20 @@
 public class Game {
+
+    private boolean isFirst;
+    private Player player1;
+    private Player player2;
+    private Display display;
+    private GameLogic gameLogic;
+
+    public Game(GameLogic gameLogic, Display display, Menu menu) {
+        this.gameLogic = gameLogic;
+        this.display = display;
+
+    }
+
+    public void run() {
+        boolean noActionsPlayer1 = false;
+        boolean noActionsPLayer2 = false;
+
+    }
 }
