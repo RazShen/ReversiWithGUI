ea066c39cb04c641dc277e822e8b8b7f05857454
Raz <shenkmanraz@gmail.com>
2018-01-08 23:06:11 +0200

    Game logic

--- src/Board.java
+++ src/Board.java
@@ -12,7 +12,7 @@ public class Board {
         this.startingColor = startingColor;
         this.notStartingColor = notStartingColor;
         for (int i = 0; i < size; i++) {
-            for (int j = 0; i < size; j++) {
+            for (int j = 0; j < size; j++) {
                 this.gamePieces[i][j] = new GamePiece();
             }
         }
