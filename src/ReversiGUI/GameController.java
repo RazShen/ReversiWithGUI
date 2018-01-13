fc9f1671e64b7ea750fe8e7828e28aa8e056e2ca
Raz <shenkmanraz@gmail.com>
2018-01-13 16:53:15 +0200

    game run

--- src/ReversiGUI/GameController.java
+++ src/ReversiGUI/GameController.java
@@ -40,6 +40,9 @@ public class GameController implements Initializable {
     @FXML
     private Label player2Score;
 
+    @FXML
+    private Label extraMessage;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.noMoreActionsP1 = false;
@@ -61,15 +64,17 @@ public class GameController implements Initializable {
         root.setAlignment(Pos.TOP_LEFT);
         guiBoard.draw();
         VBox gameStatus = new VBox();
-        currentPlayer = new Label("\nCurrent player: First");
+        currentPlayer = new Label("\nCurrent player: Player 1");
         player1Score = new Label("First player score: 2");
         player2Score = new Label("Second player score: 2");
         message = new Label("Player 1:\nIt's your move!");
+        extraMessage = new Label ("");
+        extraMessage.setFont(new Font(15));
         message.setFont(new Font(15));
         root.setAlignment(Pos.TOP_LEFT);
         root.setSpacing(20);
         gameStatus.setSpacing(10);
-        gameStatus.getChildren().addAll(currentPlayer, player1Score, player2Score, message);
+        gameStatus.getChildren().addAll(currentPlayer, player1Score, player2Score, message, extraMessage);
         root.getChildren().add(gameStatus);
         this.isPlayer1 = true;
         this.gameLogic = new RegularGameLogic(this.board, Color.web(this.player1Color), Color.web(this.player2Color));
@@ -81,6 +86,20 @@ public class GameController implements Initializable {
 
     @FXML
     protected void singleMove(Pair move) {
+        int firstScore = gameLogic.getFirstPlayerScore();
+        int secondScore = gameLogic.getSecondPlayerScore();
+
+        if (this.board.isBoardFull() || (this.noMoreActionsP2 && this.noMoreActionsP1)) {
+            if (firstScore > secondScore) {
+                message.setText("Game Over\nFirst player wins!");
+            } else if (secondScore > firstScore) {
+                message.setText("Game Over\nSecond player wins!");
+            } else {
+                message.setText("Game Over\nIt's a Tie!");
+            }
+        }
+
+
         int moves = 0;
         Pair pArr[] = new Pair[this.gameLogic.getBoardSize() * this.gameLogic.getBoardSize() + 1];
         if (this.isPlayer1) {
@@ -88,8 +107,23 @@ public class GameController implements Initializable {
         } else {
             moves = gameLogic.possibleMoves(pArr, moves, Color.web(this.player2Color));
         }
-        int firstScore = gameLogic.getFirstPlayerScore();
-        int secondScore = gameLogic.getSecondPlayerScore();
+
+
+        if (moves == 0) {
+            if (this.isPlayer1) {
+                message.setText("Player 1:\nYou have no more moves!");
+                this.isPlayer1 = false;
+            } else {
+                message.setText("Player 2:\nYou have no more moves!");
+                this.isPlayer1 = true;
+            }
+            if (this.isPlayer1) {
+                this.noMoreActionsP1 = true;
+            } else {
+                this.noMoreActionsP2 = true;
+            }
+        }
+
         boolean validMove = this.gameLogic.checkInput(move, pArr, moves);
 
         if (validMove) {
@@ -97,34 +131,31 @@ public class GameController implements Initializable {
                 this.gameLogic.flipCell(move, Color.web(this.player2Color), Color.web(this.player1Color));
                 noMoreActionsP1 = false;
                 this.isPlayer1 = false;
-//
-//
+
 //                pArr = new Pair[this.gameLogic.getBoardSize() * this.gameLogic.getBoardSize() + 1];
 //                moves = gameLogic.possibleMoves(pArr, moves, Color.web(this.player2Color));
 //                if(moves == 0) {
 //                    this.isPlayer1 = true;
+//                    this.extraMessage.setText("No moves for:\nPlayer 2");
 //                }
-
-            }
-            else {
+            } else {
                 this.gameLogic.flipCell(move, Color.web(this.player1Color), Color.web(this.player2Color));
                 noMoreActionsP2 = false;
                 this.isPlayer1 = true;
-//
-//
+
 //                pArr = new Pair[this.gameLogic.getBoardSize() * this.gameLogic.getBoardSize() + 1];
 //                moves = gameLogic.possibleMoves(pArr, moves, Color.web(this.player1Color));
 //                if(moves == 0) {
-//
 //                    this.isPlayer1 = false;
+//                    this.extraMessage.setText("No moves for:\nPlayer 1");
 //                }
 
             }
             if (!isPlayer1) {
-                currentPlayer.setText("\nCurrent Player: Second");
+                currentPlayer.setText("\nCurrent Player: Player 2");
                 message.setText("Player 2:\nIt's your move!");
             } else {
-                currentPlayer.setText("\nCurrent Player: First");
+                currentPlayer.setText("\nCurrent Player: Player 1");
                 message.setText("Player 1:\nIt's your move!");
             }
 //            board.setPossibleMoves(logic.getPossibleMoves());
@@ -132,8 +163,8 @@ public class GameController implements Initializable {
             guiBoard.draw();
             firstScore = gameLogic.getFirstPlayerScore();
             secondScore = gameLogic.getSecondPlayerScore();
-            player1Score.setText("First player's score: " + firstScore);
-            player2Score.setText("Second Player's score : " + secondScore);
+            player1Score.setText("Player 1 score: " + firstScore);
+            player2Score.setText("Player 2 score: " + secondScore);
 
         } else {
             if (this.board.isBoardFull() || (this.noMoreActionsP2 && this.noMoreActionsP1)) {
@@ -146,11 +177,16 @@ public class GameController implements Initializable {
                 }
             } else if (moves == 0) {
                 if (this.isPlayer1) {
-                    message.setText("Player 1:\nYou have no more moves!");
+                    message.setText("Player 1:\nYou have\nno more moves!");
                     this.isPlayer1 = false;
                 } else {
-                    message.setText("Player 2:\nYou have no more moves!");
+                    message.setText("Player 2:\nYou have\nno more moves!");
                     this.isPlayer1 = true;
+                }
+                if (this.isPlayer1) {
+                    this.noMoreActionsP1 = true;
+                } else {
+                    this.noMoreActionsP2 = true;
                 }
             } else {
                 message.setText("That not a valid move");
